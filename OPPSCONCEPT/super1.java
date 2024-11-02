7ff0f92cfe6d8ad25856a7db0d8a049558cd77e8
birju0451 <birjukumar1252005@gmail.com>
2024-11-02 19:16:21 +0530

    first comment

---
+++ OPPSCONCEPT/super1.java
@@ -0,0 +1,20 @@
+//whenever you get object of a class it will call the constructor of subclass and super class both ,and here A is super class and B is the subclas 
+class A{
+    public A(){
+        System.out.println("in A");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("in B");
+    }
+}
+
+    
+public class super1 {
+    
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        B obj =new B();
+    }
+}
---
+++ OPPSCONCEPT/super2.java
@@ -0,0 +1,29 @@
+
+//1.whenever you get object of a class it will call the constructor of subclass and super class both ,and here A is super class and B is the subclas 
+//2.every constructor in java have a method which is there there even if we don't mension and that method is called super class
+class A{
+    public A(){
+        System.out.println("in A");
+    }
+    public A(int n){
+        System.out.println("in A int");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("in B");
+    }
+
+    public B(int n){
+        System.out.println("in B int ");
+    }
+}
+
+    
+public class super2 {
+    
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        B obj =new B(10);
+    }
+}
