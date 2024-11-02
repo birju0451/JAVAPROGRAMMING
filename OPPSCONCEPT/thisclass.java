026c5315a2fc32cd38ee485dba169541f88e446f
birju0451 <birjukumar1252005@gmail.com>
2024-11-02 23:20:47 +0530

    .

--- OPPSCONCEPT/thisclass.java
+++ OPPSCONCEPT/thisclass.java
@@ -1,4 +1,6 @@
-
+/*
+ * this:-this method execute the constructor of the same class and "super()" method excute the constructor of the super class or parent class  
+ */
 //1.whenever you get object of a class it will call the constructor of subclass and super class both ,and here A is super class and B is the subclas 
 //2.every constructor in java have a method which is there there even if we don't mension and that method is called super class
 class A{
@@ -13,21 +15,21 @@ class A{
 }
 class B extends A{
     public B(){
-        super(10);
+        super();
         System.out.println("in B");
     }
 
     public B(int n){
-        super(10);
+        this();
         System.out.println("in B int ");
     }
 }
 
     
-public class super3 {
+public class thisclass {
     
     @SuppressWarnings("unused")
     public static void main(String[] args) {
-        B obj =new B();
+        B obj =new B(5);
     }
 }
