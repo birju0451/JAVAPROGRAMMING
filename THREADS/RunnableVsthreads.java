e91f8140947584360ba0ce8d59f1b266bdd4345c
birju0451 <birjukumar1252005@gmail.com>
2024-11-04 22:20:37 +0530

    first comment

--- THREADS/RunnableVsthreads.java
+++ THREADS/RunnableVsthreads.java
@@ -1,8 +1,9 @@
 package THREADS;
 
-class A extends Thread{
+
+class A implements Runnable{
     public void run(){
-        for(int i=0;i<=50;i++){
+        for(int i=0;i<=5;i++){
             System.out.println("hii");
             try {
                 Thread.sleep(10);
@@ -13,9 +14,9 @@ class A extends Thread{
     }
 }
 
-class B extends Thread{
+class B implements Runnable{
     public void run(){
-        for(int i=0;i<=50;i++){
+        for(int i=0;i<=5;i++){
             System.out.println("hello");
             try {
                 Thread.sleep(10);
@@ -25,20 +26,18 @@ class B extends Thread{
         }
     }
 }
-public class MultipleThreads {
+
+
+public class RunnableVsthreads {
     public static void main(String[] args) {
         A obj1=new A();
         B obj2=new B();
 
         //System.out.println(obj1.getName());
         //obj2.setPriority(Thread.MAX_PRIORITY);
-        
-        obj1.start();
-        try {
-            Thread.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        obj2.start();
+        Thread t1=new Thread();
+        Thread t2= new Thread();
+        t1.start();
+        t2.start();
     }
 }
